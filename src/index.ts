bb0b97b8a5da60a7398bc559cacdbf4779f0473c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2024-02-12 19:19:34 -0300

    Setup inicial

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log("OOOI");
+console.log("OOOI");
+console.log("OOOI");
+console.log("OOOI");
+console.log("OOOI");
+
+function ola(): void {
+  const oi = "oi";
+  console.log(oi);
+}
+ola();
